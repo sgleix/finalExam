2a964a438102271fd82b196c8157bebbe6132c9a
Hannah Ramsden <114274438+HKRamsden@users.noreply.github.com>
2023-04-29 12:21:56 -0400

    Updated files

--- serverWithMenu.c
+++ serverWithMenu.c
@@ -1,71 +1,72 @@
-#include <stdio.h>      
-#include <sys/socket.h> 
-#include <arpa/inet.h>  
-#include <stdlib.h>     
-#include <string.h>    
-#include <unistd.h>    
+#include <stdio.h>       /* for printf() and fprintf() */
+#include <sys/socket.h>  /* for socket(), bind(), and connect() */
+#include <arpa/inet.h>   /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>      /* for atoi() */
+#include <string.h>      /* for memset() */
+#include <unistd.h>      /* for close() */
+#include <dirent.h>      /* for directory listing */
+#include <sys/wait.h>    /* for waitpid() */
+#define RCVBUFSIZE 1024  /* Size of receive buffer */
 
-// Maximum number of pending connections
-#define MAXPENDING 5   
+void DieWithError(char *errorMessage);  /* Error handling function */
+void HandleTCPClient(int clntSocket);   /* TCP client handling function */
 
-// Function prototypes
-void DieWithError(char *errorMessage);  
-void HandleTCPClient(int clntSocket);   
-
-// Main function
 int main(int argc, char *argv[])
 {
-    int servSock;                    // Server socket
-    int clntSock;                    // Client socket
-    struct sockaddr_in echoServAddr; // Server address
-    struct sockaddr_in echoClntAddr; // Client address
-    unsigned short echoServPort;     // Server port
-    unsigned int clntLen;            // Length of client address data structure
+    int servSock;                    /* Socket descriptor for server */
+    int clntSock;                    /* Socket descriptor for client */
+    struct sockaddr_in echoServAddr; /* Local address */
+    struct sockaddr_in echoClntAddr; /* Client address */
+    unsigned short echoServPort;     /* Server port */
+    unsigned int clntLen;            /* Length of client address data structure */
+    pid_t pid;                       /* Process ID for forked child process */
 
-    // Check if the number of arguments is correct
-    if (argc != 2)     
+    if (argc != 2)     /* Test for correct number of arguments */
     {
-        fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <Server Port>\n", argv[0]);
         exit(1);
     }
 
-    // Get server port from command line argument
-    echoServPort = atoi(argv[1]);  
+    echoServPort = atoi(argv[1]);  /* First arg: local port */
 
-    // Create a TCP socket
+    /* Create socket for incoming connections */
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
-      
-    // Initialize server address structure
-    memset(&echoServAddr, 0, sizeof(echoServAddr));   
-    echoServAddr.sin_family = AF_INET;                
-    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    echoServAddr.sin_port = htons(echoServPort);     
 
-    // Bind the socket to the local server address
+    /* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
+    echoServAddr.sin_family = AF_INET;                /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
+
+    /* Bind to the local address */
     if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
         DieWithError("bind() failed");
 
-    // Start listening for incoming connections
-    if (listen(servSock, MAXPENDING) < 0)
+    /* Mark the socket so it will listen for incoming connections */
+    if (listen(servSock, 5) < 0)
         DieWithError("listen() failed");
 
-    // Run the server loop
     for (;;) /* Run forever */
     {
-        // Set the size of the client address structure
+        /* Set the size of the in-out parameter */
         clntLen = sizeof(echoClntAddr);
 
-        // Accept a new incoming client connection
-        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
-                               &clntLen)) < 0)
+        /* Wait for a client to connect */
+        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, &clntLen)) < 0)
             DieWithError("accept() failed");
 
-        // Print the client's IP address
-        printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
-
-        // Call the function to handle the client connection
-        HandleTCPClient(clntSock);
+        /* Fork child process to handle client */
+        if ((pid = fork()) < 0)
+            DieWithError("fork() failed");
+        else if (pid == 0)  /* If this is the child process */
+        {
+            close(servSock);   /* Child closes parent socket */
+            HandleTCPClient(clntSock);
+            exit(0);           /* Child process terminates */
+        }
+        else    /* If this is the parent process */
+            close(clntSock); /* Parent closes child socket (important!) */
     }
     /* NOT REACHED */
 }
