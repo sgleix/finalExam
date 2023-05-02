948da0d47928174a7ecfc425d3450b3610ccfef3
Hannah Ramsden <114274438+HKRamsden@users.noreply.github.com>
2023-05-02 16:39:43 -0400

    reee

--- HandleTCPClientWithMenu.c
+++ HandleTCPClientWithMenu.c
@@ -1,134 +1,291 @@
-#include <stdio.h>      
-#include <sys/socket.h> 
-#include <unistd.h>     
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys.types.h>35
+#include <dirent.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <libgen.h>
 
-#define RCVBUFSIZE 32   
-#define NAME_SIZE 21 
+#define RCVBUFSIZE 32
+#define NAME_SIZE 32
+#define MAX_FILENAME_LENGTH 256
+#define MAX_LINE_LENGTH 1024
 
-// Define a struct for the menu
-struct menu{
-  unsigned char line1[20];
-  unsigned char line2[20];
-  unsigned char line3[20];
+struct stat mystat, *sp;
+char * t1 ="xwrxwrxwr-------";
+char * t2 ="----------------";
+
+struct menu {
+    unsigned char line1[20];
+    unsigned char line2[20];
+    unsigned char line3[20];
 } men;
 
-void DieWithError(char *errorMessage); 
+void DieWithError(char *errorMessage);
 void get(int, void *, unsigned int);
 void put(int, void *, unsigned int);
 unsigned int sendMenuAndWaitForResponse(int);
-void askForName(int sock, char *, unsigned int);
-void doSomethingWithName(char *);
-void askForNumber(int sock, int *, unsigned int);
-void doSomethingWithNumber(int);
+void askForFileName(int sock, char *, unsigned int);
+long findSize(FILE * fp);
+void sendFileToClient(char * filename, int sock);
+//void handleGetRequest(int);
+//void handleDirectoryRequest(int);
+void HandleTCPClient(int);
+void ls_dir2(char * dname, char * buffer);
 
-// Main function to handle the client's requests
-void HandleTCPClient(int clntSocket)
-{
-    int recvMsgSize;                   
-    unsigned int response = 0;
-    unsigned char name[NAME_SIZE]; 
-    int number = 0;
-    unsigned char errorMsg[] = "Invalid Choice";
-    unsigned char bye[] = "Bye!";
 
-    // Send the menu to the client and wait for their response
-    response = sendMenuAndWaitForResponse(clntSocket);
+//NASTY MF!
+/*int main(int argc, char *argv[]) {
+    int servSock;
+    int clntSock;
+    unsigned short servPort;
+    pid_t processID;
 
-    // Continue handling requests until the client chooses to exit (option 3)
-    while(response != 3)
-    {
-        switch(response)
-        {
-            case 1: // Client selected option 1 (name)
-                printf("Client selected 1.\n");
-                // Ask the client for their name and store it in the 'name' variable
-                askForName(clntSocket, name, NAME_SIZE);
-                // Process the received name
-                doSomethingWithName(name);
-                break;
-            case 2: // Client selected option 2 (number)
-                printf("Client selected 2.\n");
-                // Ask the client for a number and store it in the 'number' variable
-                askForNumber(clntSocket, &number, sizeof(int));
-                // Process the received number
-                doSomethingWithNumber(number);
-                break;
-            default: // Client selected an invalid option
-                printf("Client selected junk.\n");
-                // Send an error message to the client
-                put(clntSocket, errorMsg, sizeof(errorMsg));
-                break;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <Server Port>\n", argv[0]);
+        exit(1);
+    }
+
+    servPort = atoi(argv[1]);
+
+    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        DieWithError("socket() failed");
+    }
+
+    struct sockaddr_in servAddr;
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(servPort);
+
+    if (bind(servSock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        DieWithError("bind() failed");
+    }
+
+    if (listen(servSock, 5) < 0) {
+        DieWithError("listen() failed");
+    }
+
+    for (;;) {
+        struct sockaddr_in clntAddr;
+        unsigned int clntLen = sizeof(clntAddr);
+
+        if ((clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntLen)) < 0) {
+            DieWithError("accept() failed");
         }
-        // Send the menu to the client again and wait for their response
-        response = sendMenuAndWaitForResponse(clntSocket);
-    }//end while
 
-    // Send a goodbye message to the client and close the connection
-    put(clntSocket, bye, sizeof(bye));
-    close(clntSocket);    
-    printf("Connection with client %d closed.\n", clntSocket);
+        if ((processID = fork()) < 0) {
+            DieWithError("fork() failed");
+        } else if (processID == 0) {
+            close(servSock);
+            HandleTCPClient(clntSock);
+            exit(0);
+        }
+
+        close(clntSock);
+        while (waitpid(-1, NULL, WNOHANG) > 0);
+    }
+
+    return 0;
+} */
+
+
+void DieWithError(char *errorMessage) {
+    perror(errorMessage);
+    exit(1);
 }
 
-// Function to send a menu to the client and wait for their response
+
+void get(int sock, void *buffer, unsigned int bufferSize)
+{
+    int totalBytesReceived = 0;
+    int bytesReceived = 0;
+
+    while (totalBytesReceived < bufferSize) {
+	    bytesReceived = recv(sock, buffer + totalBytesReceived, bufferSize - totalBytesReceived, 0);
+	    if (bytesReceived < 0) 
+	    {
+		DieWithError("recv() failed");
+	    } else if (bytesReceived == 0) {
+		    DieWithError("Connection closed prematurely");
+	    }
+	    totalBytesreceived += bytesReceived;
+    }
+}
+
+
+void put(int sock, void *buffer, unsigned int bufferSize) {
+    int toatlBytesSent = 0;
+    int bytesSent = 0;
+
+    while (totalBytesSent < bufferSize) {
+	    bytesSent = send(sock, buffer + totalBytesSent, bufferSize - totalBytesSent, 0);
+	    if (bytesSent < 0) {
+		    DieWithError("send() failed\n");
+	    }
+	    toatlBytesSent += bytesSent;
+    }
+}
+
+void ls_dir2(char * dname, char * dst) {
+	DIR *dp;
+	struct dirent *dirp;
+	dp = opendir(dname);
+	while((dirp = readdir(dp)) != null) {
+		strcat(dst, dirp->dname);
+		strcat(dst, "\n");
+	}
+	closedir(dp);
+}
+
+void sendFileToClient(char * filename, int sock) 
+{
+	long fileSize;
+	FILE* fp = fopen(filename, "r");
+	if (fp == NULL) {
+		DieWithError("File not found");
+	}
+	fileSize = findSize(fp);
+	int size = htonl(fileSize);
+	put(sock, &size, sizeof(fileSize));
+	char * buffer = (char *)malloc(sizeof(char) * fileSize);
+	fread(buffer, sizeof(char), fileSize, fp);
+	printf("%s\n", buffer);
+	put(sock, buffer, fileSize);
+	fclose(fp);
+}
+
+
 unsigned int sendMenuAndWaitForResponse(int clntSocket)
 {
     struct menu mainMenu;
     unsigned int response = 0;
-    // Initialize the mainMenu struct with the menu options
-    memset(&mainMenu, 0, sizeof(struct menu));  
-    strcpy(mainMenu.line1,"1) Enter name\n");
-    strcpy(mainMenu.line2, "2) Enter number\n");
+    memset(&mainMenu, 0, sizeof(struct menu)); /* Zero out structure */
+    strcpy(mainMenu.line1,"1) Select a file\n");
+    strcpy(mainMenu.line2, "2) list available files\n");
     strcpy(mainMenu.line3, "3) Quit\n");
-    // Send the menu to the client
     printf("Sending menu\n");
     put(clntSocket, &mainMenu, sizeof(struct menu));
-    // Receive the client's response
     get(clntSocket, &response, sizeof(unsigned int));
-    // Convert the response from network byte order to host byte order and return it
     return ntohl(response);
 }
 
-// Function to ask the client for their name and store it in the provided buffer
-void askForName(int sock, char * name, unsigned int size)
+void askForFileName(int socket)
 {
-    unsigned char msg[21];
-    // Initialize the message buffer and set the message content
-    memset(msg, 0, sizeof(msg));
-    strcpy(msg, "Enter name:\n");
-    // Send the message to the client
-    put(sock, msg, sizeof(msg));
-    // Receive the client's name and store it in the provided buffer
-    memset(name, 0, NAME_SIZE);
-    get(sock, name, NAME_SIZE);
+   unsigned char msg[80];
+   memset(msg, 0, sizeof(msg));
+   strcpy(msg, "Enter file name from list of directories:\n");
+   put(sock, msg, sizeof(msg));
+   memset(name, 0, NAME_SIZE);
+   get(sock, name, NAME_SIZE);
+   printf("Received file name from the client: %s\n", name);
 }
 
-// Function to process the received name
-void doSomethingWithName(char * name)
+
+/*void handleGetRequest(int clntSocket) {
+    // Ask for the file name
+    char* fileName = askForFileName(clntSocket);
+
+    // Open the file
+    FILE* fp = fopen(fileName, "rb");
+    if (fp == NULL) {
+        // File not found
+        DieWithError("File not found");
+    }
+
+    // Send the file contents
+    int fileSize = sendFile(fp, clntSocket);
+
+    // Log the successful transfer
+    printf("File '%s' (%d bytes) sent successfully\n", fileName, fileSize);
+
+    // Free memory and close file pointer
+    free(fileName);
+    fclose(fp);
+} */
+
+//girl what
+/*void handleDirectoryRequest(int sock) {
+    DIR *dir;
+    struct dirent *entry;
+    char *dir_name = ".";
+    int num_entries = 0;
+    char buffer[MAX_FILENAME_LENGTH];
+    int buffer_len = 0;
+
+    if ((dir = opendir(dir_name)) == NULL) {
+        DieWithError("opendir() failed");
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] != '.') {
+            num_entries++;
+            buffer_len += snprintf(buffer + buffer_len, MAX_FILENAME_LENGTH - buffer_len, "%s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+
+    if (num_entries == 0) {
+        strcpy(buffer, "Directory is empty.\n");
+    }
+
+    put(sock, buffer, strlen(buffer));
+} */
+
+
+
+void handleTCPClient(int clntSocket)
 {
-    printf("Received name from the client: %s\n", name);
+    char menu[] = "\nPlease choose an option:\n1. Get directory listing\n2. Select a file\n3. Quit\n";
+    char recvBuffer[RCVBUFSIZE];
+    int recvMsgSize;
+    unsigned char name[NAME_SIZE]; // max length 
+    unsigned int option = 0;
+    int quitFlag = 0;
+    unsigned char errorMsg[] = "Invalid Choice!";
+    unsigned char bye{} = "Bye!";
+
+    while (!quitFlag) {
+        // Send menu to client
+        sendMenuAndWaitForResponse(clntSocket, menu, &option);
+
+        switch (option) {
+        case 1:
+	    printf("Client chose 1\n");
+            //handleDirectoryRequest(clntSocket);
+	    askForFileName(clntSocket,name, sizeof(name));
+	    sendFiletoClient(name, clntSocket);
+            break;
+        case 2:
+	    printf("Client chose 2\n");
+            //handleGetRequest(clntSocket);
+	    char dirbuffer[5000];
+	    char cwd[1024];
+	    getcwd(cwd, sizeof(cwd));
+	    memset(dirbuffer, 0, sizeof(dirbuffer));
+            break;
+        case 3:
+            quitFlag = 1;
+            break;
+        default:
+            DieWithError("Invalid option selected.");
+	    //NOT supposed to do this!!
+            break;
+        }
+	option = sendMenuAndWaitForResponse(clntSocket);
+    } //ends the while loop
+
+    put(clntSocket, bye, sizeof(bye));
+    close(clntSocket);
+    printf("Connection with client %d closed.\n", clntSocket);
+
+    close(clntSocket); // Close client socket
 }
 
-// Function to ask the client for a number and store it in the provided integer pointer
-void askForNumber(int sock, int * numPtr, unsigned int size)
-{
-    unsigned char msg[21];
-    int numIn = 0;
-
-    // Initialize the message buffer and set the message content
-    memset(msg, 0, sizeof(msg));
-    strcpy(msg, "Enter number:\n");
-    // Send the message to the client
-    put(sock, msg, sizeof(msg));
-    // Receive the client's number
-    get(sock, &numIn, sizeof(int));
-    // Convert the received number from network byte order to host byte order and store it in the provided integer pointer
-    *numPtr = ntohl(numIn);
-}
-
-// Function to process the received number
-void doSomethingWithNumber(int number)
-{
-    printf("Received number from the client: %d\n", number);
-}
